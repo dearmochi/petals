a0a3d3a26e3289cb3a8746a44dcfb3f8c16dc883
mochi <1496804+dearmochi@users.noreply.github.com>
2021-09-03 14:52:51 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Petals",
+      template: "./src/index.html"
+    })
+  ]
+}
